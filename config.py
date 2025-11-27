2023e92b2ec887cdefb4b8de1999236b34808ae4
erichategekimana <h.eric@alustudent.com>
2025-11-27 06:39:18 +0000

    all secret keys move to os.envronment

--- config.py
+++ config.py
@@ -3,12 +3,11 @@ from datetime import timedelta
 
 
 class Config:
-    SECRET_KEY = "This_is_my_secret_key"
-    JWT_SECRET = "This_is_my_secret_key"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    JWT_SECRET = os.environ.get("JWT_SECRET")
     JWT_ALGORITHM = "HS256"
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)  # 1 hour default
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://Bbuddy_user:Eric_123@127.0.0.1:3306/Bbuddy_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GEMINI_API_KEY = "AIzaSyB0Ed8oBdqwIgLfjn4uzXreH4xZu8sXWWo"
 
-GEMINI_API_KEY = ""
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
